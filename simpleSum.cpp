fc6b002fa89096ea61a85c940ff7a903d772ec33
Rafał Ściebura <rafal.sciebura7@gmail.com>
2022-07-01 00:28:59 +0200

    Add new files

---
+++ simpleSum.cpp
@@ -0,0 +1,37 @@
+﻿
+#include <iostream>
+
+int main() {
+
+    int i;
+    int j;
+    int t;
+    int n;
+    int sum;
+
+    std::cout << "Type the number of tests: ";
+    std::cin >> t;
+
+    for (i = 0; i < t; i++) {
+        
+        int numbers = 1;
+
+        std::cout << "How many numbers: ";
+        std::cin >> n;
+        sum = 0;
+        
+
+        for (j = 0; j < n; j++) {
+            int temp;
+
+            std::cout << "Number " << numbers << ": ";
+            std::cin >> temp;
+            
+            sum += temp;
+            numbers++;
+        }
+        std::cout << "" << sum << std::endl;
+    }
+
+    return 0;
+}
